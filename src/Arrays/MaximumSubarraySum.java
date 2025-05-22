41ed5d7f003862964f1a3fd9dd26b2542da45b9d
ash <akashganga333@gmail.com>
2025-05-23 01:39:56 +0530

    maximum sum sub array.

--- src/Arrays/MaximumSubarraySum.java
+++ src/Arrays/MaximumSubarraySum.java
@@ -10,12 +10,22 @@ public class MaximumSubarraySum {
     public static void main(String[] args) {
         int [] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
         // Kadane Algorithm
-        int sum = 0, maxSum = Integer.MIN_VALUE;
+        int sum = 0, maxSum = Integer.MIN_VALUE, start = 0, startIndex = 0, endIndex = 0;
         for (int i = 0; i < arr.length; i++) {
+            if(sum == 0) start = i;
             sum += arr[i];
+            if(sum>maxSum){
+                startIndex = start;
+                endIndex = i;
+            }
             maxSum = Math.max(maxSum, sum);
             if(sum<0) sum = 0;
         }
+        // print max sum of subarray
         System.out.println(maxSum);
+        // print the maximum sum subarray
+        for(int i = startIndex; i <= endIndex; i++){
+            System.out.print(arr[i] + " ");
+        }
     }
 }

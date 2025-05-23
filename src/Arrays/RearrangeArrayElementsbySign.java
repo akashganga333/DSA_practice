8c785bce5fee2a0587083fdd0eb89291726162ab
ash <akashganga333@gmail.com>
2025-05-23 13:16:42 +0530

    rearrange array by sign

---
+++ src/Arrays/RearrangeArrayElementsbySign.java
@@ -0,0 +1,37 @@
+package Arrays;
+
+//You are given a 0-indexed integer array nums of even length consisting of an equal number of positive and negative integers.
+//You should return the array of nums such that the the array follows the given conditions:
+//Every consecutive pair of integers have opposite signs.
+//For all integers with the same sign, the order in which they were present in nums is preserved.
+//The rearranged array begins with a positive integer.
+//Return the modified array after rearranging the elements to satisfy the aforementioned conditions.
+//Input: nums = [3,1,-2,-5,2,-4]
+//Output: [3,-2,1,-5,2,-4]
+//Explanation:
+//The positive integers in nums are [3,1,2]. The negative integers are [-2,-5,-4].
+//The only possible way to rearrange them such that they satisfy all conditions is [3,-2,1,-5,2,-4].
+//Other ways such as [1,-2,2,-5,3,-4], [3,1,2,-2,-5,-4], [-2,3,-5,1,-4,2] are incorrect because they do not satisfy one or more conditions.
+
+public class RearrangeArrayElementsbySign {
+    public static void main(String[] args) {
+        int arr[] = {3,1,-2,-5,2,-4};
+        int res[] = new int[arr.length];
+        int p = 0, n = 1, i = 0;
+        while( i < arr.length) {
+            if(arr[i]>0){
+                res[p] = arr[i];
+                p+=2;
+            }
+            else if(arr[i]<0) {
+                res[n] = arr[i];
+                n+=2;
+            }
+            i++;
+        }
+
+        for(int nums:res){
+            System.out.print(nums+" ");
+        }
+    }
+}
